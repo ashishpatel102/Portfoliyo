defa62e44d8d4493f7dae641a1f32e9aa41ebe73
Ashish Patel <ashishpatel84h@gmail.com>
2025-06-28 00:20:34 +0530

    Update auth.js

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -49,7 +49,7 @@ const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: false, // ✅ true if using https
-      sameSite: "Lax",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
